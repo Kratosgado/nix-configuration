654f3c5712456acf5d7abd975dff9296006f7710
Kratosgado <mbeahessilfieprince@gmail.com>
2025-06-12 00:15:43 +0000

    bling config

--- nixvim/cmp/blink.nix
+++ nixvim/cmp/blink.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... }: {
+{
   plugins = {
     friendly-snippets.enable = true;
+    blink-cmp-dictionary.enable = true;
+    blink-emoji.enable = true;
     blink-cmp = {
       enable = true;
       settings = {
@@ -20,24 +22,50 @@
           };
           menu.border = "solid";
         };
-        keymap = { preset = "super-tab"; };
+
+        keymap = {
+          preset = "super-tab";
+          "<Tab>" = [ "snippet_forward" "select_next" "fallback" ];
+          "<S-Tab>" = [ "snippet_backward" "select_prev" "fallback" ];
+          "CR" = [ "select_and_accept" "fallback" ];
+          "<C-k>" = [ "show_signature" "hide_signature" "fallback" ];
+        };
         signature = { enabled = true; };
         sources = {
           # cmdline = [ ];
-          default = [ "lsp" "path" "snippets" "buffer" ];
-          per_filetype = { sql = [ "snippets" "dadbod" "buffer" ]; };
+          default = [ "lsp" "path" "snippets" "buffer" "dictionary" ];
+          per_filetype = {
+            sql = [ "snippets" "dadbod" "buffer" ];
+            vue = [ "emoji" ];
+            tsx = [ "emoji" ];
+            html = [ "emoji" ];
+          };
           providers = {
             # buffer = { score_offset = -7; };
             dadbod = {
               name = "Dadbod";
               module = "vim_dadbod_completion.blink";
             };
+
+            emoji = {
+              module = "blink-emoji";
+              name = "Emoji";
+              score_offset = 15;
+              # Optional configurations
+              opts = { insert = true; };
+            };
+            dictionary = {
+              module = "blink-cmp-dictionary";
+              name = "Dict";
+              score_offset = 100;
+              min_keyword_length = 3;
+              # Optional configurations
+              opts = { };
+            };
             # lsp = { fallbacks = [ ]; };
           };
         };
       };
-
     };
   };
-
 }
