315c5a15bcc2a9f6ff741fda4c4e05d91ee329d6
Kratosgado <mbeahessilfieprince@gmail.com>
2025-06-21 10:50:09 +0000

    user settings vscode

--- vscode/default.nix
+++ vscode/default.nix
@@ -1,4 +1,5 @@
 { pkgs, ... }: {
+
   programs.vscode = {
     enable = true;
     profiles = {
@@ -10,7 +11,35 @@
           adpyke.codesnap
           dbaeumer.vscode-eslint
           usernamehw.errorlens
+          # (vscode-with-extensions.override {
+          #   extensions = [ "publisher.extension-name" ];
+          # })
+          # (pkgs.vscode-with-extensions.override {
+          #   vscodeExtensions = with pkgs.vscode-extensions;
+          #     [
+          #       bbenoist.nix
+          #       ms-python.python
+          #       ms-azuretools.vscode-docker
+          #       ms-vscode-remote.remote-ssh
+          #     ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+          #       {
+          #         name = "italic-dark-theme";
+          #         publisher = "Kratosgado";
+          #         version = "0.0.2";
+          #         # rev = "87105593033575af55810ed42c1eef1bc05d1037";
+          #         sha256 = "QUlX7Psg+090w9nvpwaRrcWlYrZ6KlgOvzCrYl1nFc8=";
+          #       }
+          #       {
+          #         name = "remote-ssh-edit";
+          #         publisher = "ms-vscode-remote";
+          #         version = "0.47.2";
+          #         sha256 =
+          #           "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g";
+          #       }
+          #     ];
+          # })
         ];
+        userSettings = builtins.fromJSON (builtins.readFile ./typescript.json);
       };
       vue = {
         extensions = with pkgs.vscode-extensions; [
@@ -19,6 +48,7 @@
           usernamehw.errorlens
           dbaeumer.vscode-eslint
           adpyke.codesnap
+
           # vue.vscode-typescript-vue-plugin
           # vue
           # (pkgs.vscode-with-extensions.override {
